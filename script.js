f6be7de8c6cbcec30a7625b4a29cbc5fe942fdbd
eugeneanokye99 <134564162+eugeneanokye99@users.noreply.github.com>
2023-10-07 21:10:59 +0000

    social media website

---
+++ script.js
@@ -0,0 +1,7 @@
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    var navbar = document.nav;
+    navbar.classList.toggle("nav-dark-mode");
+}
