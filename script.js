867a1b30ebce831aeb4c5e0212707ec6783c71f0
eugeneanokye99 <134564162+eugeneanokye99@users.noreply.github.com>
2023-10-12 19:23:21 +0000

    updated dark mode

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function darkMode() {
     navbar_span.classList.toggle("text-dark-mode");
 
     var box1 = document.querySelector(".box1");
-    box1.classList.toggle("dark-mode");
+    box1.classList.toggle("box-dark-mode");
 
     var box2 = document.querySelector(".box2");
     box2.classList.toggle("dark-mode");
@@ -22,22 +22,22 @@ function darkMode() {
     });
 
     var box3 = document.querySelector(".box3");
-    box3.classList.toggle("dark-mode");
+    box3.classList.toggle("box-dark-mode");
 
     var text_input = box2.querySelector(".text-input");
-    text_input.classList.toggle("dark-mode");
+    text_input.classList.toggle("box-dark-mode");
 
     var tabs = box2.querySelector(".tabs");
-    tabs.classList.toggle("dark-mode");
+    tabs.classList.toggle("box-dark-mode");
 
     var post = box2.querySelector(".post");
-    post.classList.toggle("dark-mode");
+    post.classList.toggle("box-dark-mode");
 
     var container = box2.querySelectorAll(".container .post");
     container.forEach(element => {
-        element.classList.toggle("dark-mode");
+        element.classList.toggle("box-dark-mode");
     });
 
     var footer = document.querySelector("footer");
-    footer.classList.toggle("dark-mode");
+    footer.classList.toggle("box-dark-mode");
 }
